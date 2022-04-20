6ff5b495e8bea51e824b37d96e9bc7efe5e31a5a
Dusan Orlovic <duleorlovic@gmail.com>
2022-04-20 15:40:45 +0200

    Add pause helper

--- test/application_system_test_case.rb
+++ test/application_system_test_case.rb
@@ -2,4 +2,11 @@ require "test_helper"
 
 class ApplicationSystemTestCase < ActionDispatch::SystemTestCase
   driven_by :selenium, using: :chrome, screen_size: [1400, 1400]
+
+  # https://github.com/duleorlovic/minitest_rails/blob/main/test/application_system_test_case.rb
+  # you can use bye bug, but it will stop rails so you can not navigate to other
+  # pages or make another requests in chrome while testing
+  def pause
+    $stderr.write('Press CTRL+j or ENTER to continue') && $stdin.gets
+  end
 end
